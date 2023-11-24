1ae32de78e61bcca4eb71125089844e53b05616e
melly-sujakto <mellysujakto@gmail.com>
2023-11-25 02:07:42 +0700

    feat: add LogoImage widget

---
+++ app/generic/ui_kit/lib/widgets/logo_image.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ui_kit/common/constants/layout_dimen.dart';
+import 'package:ui_kit/theme/colors.dart';
+import 'package:ui_kit/utils/screen_utils.dart';
+
+class LogoImage extends StatelessWidget {
+  const LogoImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: LayoutDimen.dimen_200.w,
+      height: LayoutDimen.dimen_175.h,
+      color: CustomColors.primary.c50,
+      child: Image.asset('assets/images/logo.png'),
+    );
+  }
+}
