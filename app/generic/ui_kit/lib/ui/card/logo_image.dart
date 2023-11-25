b1a0a4ed5d4bee81815117be5210e7f87b507f67
melly-sujakto <mellysujakto@gmail.com>
2023-11-25 19:06:55 +0700

    chore: move image assets to app level

--- app/generic/ui_kit/lib/ui/card/logo_image.dart
+++ app/generic/ui_kit/lib/ui/card/logo_image.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:ui_kit/common/constants/layout_dimen.dart';
-import 'package:ui_kit/theme/colors.dart';
 import 'package:ui_kit/utils/screen_utils.dart';
 
 class LogoImage extends StatelessWidget {
@@ -8,11 +7,9 @@ class LogoImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // coba bikin flutter project tapi yg module
-    return Container(
+    return SizedBox(
       width: LayoutDimen.dimen_200.w,
       height: LayoutDimen.dimen_175.h,
-      color: CustomColors.primary.c50,
       child: Image.asset('assets/images/logo.png'),
     );
   }
