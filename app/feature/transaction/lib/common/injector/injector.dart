237d0be13082d2b24bbb4a593a996a9f0c9a7fa4
melly-sujakto <mellysujakto@gmail.com>
2023-12-02 01:43:17 +0700

    chore: comment unused injector

--- app/feature/transaction/lib/common/injector/injector.dart
+++ app/feature/transaction/lib/common/injector/injector.dart
@@ -1,13 +1,13 @@
-import 'package:library_injection/package/kiwi.dart';
+// import 'package:library_injection/package/kiwi.dart';
 
-part 'injector.g.dart';
+// part 'injector.g.dart';
 
-abstract class Injector {
-  static void init() {
-    _$Injector()._configure();
-  }
+// abstract class Injector {
+//   static void init() {
+//     _$Injector()._configure();
+//   }
 
-  static final T Function<T>([String name]) resolve = KiwiContainer().resolve;
+//   static final T Function<T>([String name]) resolve = KiwiContainer().resolve;
 
-  void _configure() {}
-}
+//   void _configure() {}
+// }
--- app/feature/transaction/lib/common/injector/injector.g.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'injector.dart';
-
-// **************************************************************************
-// KiwiInjectorGenerator
-// **************************************************************************
-
-class _$Injector extends Injector {}
