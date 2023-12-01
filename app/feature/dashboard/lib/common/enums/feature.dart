900ae66c1d85b028cf1b7d52fcb2ac93f1e0772c
melly-sujakto <mellysujakto@gmail.com>
2023-12-02 01:43:35 +0700

    chore: add feature enum

---
+++ app/feature/dashboard/lib/common/enums/feature.dart
@@ -0,0 +1,21 @@
+enum Feature {
+  sale('sale'),
+  stockIn('stockIn'),
+  product('product'),
+  stock('stock'),
+  transaction('transaction'),
+  ;
+
+  const Feature(this.value);
+  final String value;
+
+  static Feature fromString(String value) {
+    final foundEnum =
+        Feature.values.where((val) => val.value == value).firstOrNull;
+
+    if (foundEnum == null) {
+      throw UnsupportedError('Feature: $value is not supported');
+    }
+    return foundEnum;
+  }
+}
