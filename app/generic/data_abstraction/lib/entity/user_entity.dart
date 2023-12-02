d2a861d6309d6b29991010ca3cadc82a7845858e
melly-sujakto <mellysujakto@gmail.com>
2023-12-02 11:41:02 +0700

    chore: add data abstraction module

---
+++ app/generic/data_abstraction/lib/entity/user_entity.dart
@@ -0,0 +1,15 @@
+class UserEntity {
+  final String email;
+  final String name;
+  final String phone;
+  final int roleId;
+  final String storeId;
+
+  UserEntity({
+    required this.email,
+    required this.name,
+    required this.phone,
+    required this.roleId,
+    required this.storeId,
+  });
+}
---
+++ app/generic/data_abstraction/lib/model/user_model.dart
@@ -0,0 +1,21 @@
+import 'package:data_abstraction/entity/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required super.email,
+    required super.name,
+    required super.phone,
+    required super.roleId,
+    required super.storeId,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      email: json['email'],
+      name: json['name'],
+      phone: json['phone'],
+      roleId: json['roleId'],
+      storeId: json['storeId'],
+    );
+  }
+}
