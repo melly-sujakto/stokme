920ee319f06685ac6e164b07ff7517ae3f236421
melly-sujakto <mellysujakto@gmail.com>
2023-12-02 14:52:02 +0700

    chore: add some generic constants

--- app/generic/module_common/lib/common/constant/generic_constants.dart
+++ app/generic/module_common/lib/common/constant/generic_constants.dart
@@ -1,4 +1,8 @@
 class GenericConstants {
   static const String languageCode = 'language_code';
+  static const String storeId = 'store_id';
   static const String userName = 'user_name';
+  static const String email = 'email';
+  static const String phone = 'phone';
+  static const String roleId = 'role';
 }
