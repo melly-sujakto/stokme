6117cb8fa95c53ea651dbc6543943c02183ce535
melly-sujakto <mellysujakto@gmail.com>
2023-12-02 14:50:40 +0700

    chore: add strings text

--- app/feature/dashboard/lib/presentation/journey/home/home_constants.dart
+++ app/feature/dashboard/lib/presentation/journey/home/home_constants.dart
@@ -4,4 +4,12 @@ abstract class HomeStrings {
   static const productButtonTitle = 'home.button.product';
   static const stockButtonTitle = 'home.button.stock';
   static const transactionButtonTitle = 'home.button.transaction';
+  static const homeGreeting1 = 'home.greeting.1';
+  static const homeGreeting2 = 'home.greeting.2';
+  static const homeGreeting3 = 'home.greeting.3';
+  static const homeGreeting4 = 'home.greeting.4';
+}
+
+abstract class HomeAssets {
+  static const logoSmall = 'assets/images/logo_small.png';
 }
