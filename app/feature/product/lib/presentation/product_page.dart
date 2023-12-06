c962663f2f28eb22bf0eb1d72d4e4c4fea8a78fb
melly-sujakto <mellysujakto@gmail.com>
2023-12-06 23:29:06 +0700

    chore: add product detail template

--- app/feature/product/lib/presentation/product_page.dart
+++ app/feature/product/lib/presentation/product_page.dart
@@ -6,6 +6,8 @@ import 'package:module_common/i18n/i18n_extension.dart';
 import 'package:module_common/presentation/bloc/base_bloc.dart';
 import 'package:ui_kit/common/constants/layout_dimen.dart';
 import 'package:ui_kit/theme/colors.dart';
+import 'package:ui_kit/ui/button/flat_button.dart';
+import 'package:ui_kit/ui/input_field/input_basic.dart';
 import 'package:ui_kit/ui/loading_indicator/circular_progres.dart';
 import 'package:ui_kit/ui/scanner/scanner_finder.dart';
 import 'package:ui_kit/ui/tab_bar/app_tab_bar.dart';
@@ -75,7 +77,7 @@ class _ProductPageState extends State<ProductPage> {
               ),
               AppTabBar(
                 activeIndex: activeIndex,
-                onIndexChanged: (index){
+                onIndexChanged: (index) {
                   activeIndex = index;
                 },
                 items: [
@@ -132,56 +134,160 @@ class _ProductPageState extends State<ProductPage> {
           LayoutDimen.dimen_10.w,
         ),
         color: CustomColors.white,
-        child: Container(
-          padding: EdgeInsets.all(LayoutDimen.dimen_10.w),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Row(
-                children: [
-                  DummyCircleImage(title: product.name),
-                  Padding(
-                    padding: EdgeInsets.all(
-                      LayoutDimen.dimen_10.w,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          product.name,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: LayoutDimen.dimen_13.minSp,
-                            fontWeight: FontWeight.w600,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(
+            LayoutDimen.dimen_10.w,
+          ),
+          onTap: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) => detailContent(product),
+            );
+          },
+          child: Container(
+            padding: EdgeInsets.all(LayoutDimen.dimen_10.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Row(
+                  children: [
+                    DummyCircleImage(title: product.name),
+                    Padding(
+                      padding: EdgeInsets.all(
+                        LayoutDimen.dimen_10.w,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: LayoutDimen.dimen_13.minSp,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: LayoutDimen.dimen_7.h,
-                        ),
-                        Text(
-                          product.code,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: LayoutDimen.dimen_13.minSp,
-                            fontWeight: FontWeight.w100,
+                          SizedBox(
+                            height: LayoutDimen.dimen_7.h,
                           ),
-                        ),
-                      ],
+                          Text(
+                            product.code,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: LayoutDimen.dimen_13.minSp,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Text(
-                'Rp.${product.saleNet ?? '-'}',
-                style: TextStyle(
-                  fontSize: LayoutDimen.dimen_13.minSp,
-                  fontWeight: FontWeight.w300,
+                  ],
                 ),
-              ),
-            ],
+                Text(
+                  'Rp.${product.saleNet ?? '-'}',
+                  style: TextStyle(
+                    fontSize: LayoutDimen.dimen_13.minSp,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget detailContent(ProductEntity product) {
+    return Container(
+      // height: LayoutDimen.dimen_352.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(LayoutDimen.dimen_30.w),
+          topRight: Radius.circular(LayoutDimen.dimen_30.w),
+        ),
+        color: CustomColors.whiteSmoke,
+      ),
+      width: ScreenUtil.screenWidth,
+      padding: EdgeInsets.symmetric(
+        horizontal: LayoutDimen.dimen_16.w,
+      ),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: LayoutDimen.dimen_12.h,
+            ),
+            width: LayoutDimen.dimen_77.w,
+            height: LayoutDimen.dimen_7.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(
+                LayoutDimen.dimen_30.w,
+              ),
+              color: CustomColors.neutral.c90,
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: LayoutDimen.dimen_18.h,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      product.code,
+                      style: TextStyle(
+                        fontSize: LayoutDimen.dimen_18.minSp,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Icon(
+                        Icons.delete,
+                        size: LayoutDimen.dimen_30.w,
+                        color: CustomColors.neutral.c30,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: LayoutDimen.dimen_18.h,
+                ),
+                InputBasic(
+                  controller: TextEditingController(text: product.name),
+                  labelText: 'Nama',
+                  margin: EdgeInsets.zero,
+                ),
+                SizedBox(
+                  height: LayoutDimen.dimen_18.h,
+                ),
+                InputBasic(
+                  controller: TextEditingController(
+                    text: product.saleNet?.toString() ?? '',
+                  ),
+                  labelText: 'Harga Jual (opsional)',
+                  margin: EdgeInsets.zero,
+                ),
+                SizedBox(
+                  height: LayoutDimen.dimen_18.h,
+                ),
+                FlatButton(
+                  title: 'Ubah',
+                  onPressed: () {},
+                  margin: EdgeInsets.zero,
+                ),
+                SizedBox(
+                  height: LayoutDimen.dimen_18.h,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
