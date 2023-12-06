1d3716e0f272a18701d18c9a54ae2b451b7852ad
melly-sujakto <mellysujakto@gmail.com>
2023-12-07 01:18:25 +0700

    chore: move to a widget and implement it

--- app/feature/product/lib/presentation/product_page.dart
+++ app/feature/product/lib/presentation/product_page.dart
@@ -1,13 +1,13 @@
 import 'package:data_abstraction/entity/product_entity.dart';
 import 'package:feature_product/presentation/bloc/product_bloc.dart';
 import 'package:feature_product/presentation/product_constants.dart';
+import 'package:feature_product/presentation/widgets/product_detail_content.dart';
 import 'package:flutter/material.dart';
 import 'package:module_common/i18n/i18n_extension.dart';
 import 'package:module_common/presentation/bloc/base_bloc.dart';
 import 'package:ui_kit/common/constants/layout_dimen.dart';
+import 'package:ui_kit/extensions/string_extension.dart';
 import 'package:ui_kit/theme/colors.dart';
-import 'package:ui_kit/ui/button/flat_button.dart';
-import 'package:ui_kit/ui/input_field/input_basic.dart';
 import 'package:ui_kit/ui/loading_indicator/circular_progres.dart';
 import 'package:ui_kit/ui/scanner/scanner_finder.dart';
 import 'package:ui_kit/ui/snackbar/snackbar_dialog.dart';
@@ -56,7 +56,7 @@ class _ProductPageState extends State<ProductPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               ScannerFinder(
-                labelText: 'Cari nama/kode',
+                labelText: ProductStrings.scannerLabelText.i18n(context),
                 onChanged: (value) {
                   filterValue = value;
                   widget.bloc.add(
@@ -90,7 +90,7 @@ class _ProductPageState extends State<ProductPage> {
                         ),
                       );
                     },
-                    title: 'Semua',
+                    title: ProductStrings.tabItemAll.i18n(context),
                   ),
                   AppTabBarItem(
                     onTap: () {
@@ -101,7 +101,7 @@ class _ProductPageState extends State<ProductPage> {
                         ),
                       );
                     },
-                    title: 'Belum ada harga default',
+                    title: ProductStrings.tabItemUnsetPrice.i18n(context),
                   ),
                 ],
               ),
@@ -116,7 +116,8 @@ class _ProductPageState extends State<ProductPage> {
                   if (state is UpdateSuccess) {
                     SnackbarDialog().show(
                       context: context,
-                      message: 'Produk berhasil diubah',
+                      message:
+                          ProductStrings.updateSuccessSnackbar.i18n(context),
                       type: SnackbarDialogType.success,
                     );
                     widget.bloc.add(
@@ -130,7 +131,8 @@ class _ProductPageState extends State<ProductPage> {
                   if (state is UpdateFailed) {
                     SnackbarDialog().show(
                       context: context,
-                      message: 'Produk gagal diubah, silakan coba lagi',
+                      message:
+                          ProductStrings.updateFailedSnackbar.i18n(context),
                       type: SnackbarDialogType.failed,
                     );
                   }
@@ -189,7 +191,10 @@ class _ProductPageState extends State<ProductPage> {
                   ),
                 ),
               ),
-              builder: (context) => detailContent(product),
+              builder: (context) => ProductDetailContent(
+                product: product,
+                bloc: widget.bloc,
+              ),
             );
           },
           child: Container(
@@ -233,7 +238,7 @@ class _ProductPageState extends State<ProductPage> {
                   ],
                 ),
                 Text(
-                  'Rp.${product.saleNet ?? '-'}',
+                  (product.saleNet?.toString() ?? '-').toRupiahCurrency(),
                   style: TextStyle(
                     fontSize: LayoutDimen.dimen_13.minSp,
                     fontWeight: FontWeight.w300,
@@ -242,103 +247,6 @@ class _ProductPageState extends State<ProductPage> {
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget detailContent(ProductEntity product) {
-    return Padding(
-      // handle visibility of text field once keyboard shown
-      padding: EdgeInsets.only(
-        bottom: MediaQuery.of(context).viewInsets.bottom,
-      ),
-      child: Container(
-        height: LayoutDimen.dimen_352.h,
-        width: ScreenUtil.screenWidth,
-        padding: EdgeInsets.symmetric(
-          horizontal: LayoutDimen.dimen_16.w,
-        ),
-        child: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: LayoutDimen.dimen_12.h,
-              ),
-              width: LayoutDimen.dimen_77.w,
-              height: LayoutDimen.dimen_7.h,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(
-                  LayoutDimen.dimen_30.w,
-                ),
-                color: CustomColors.neutral.c90,
-              ),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    height: LayoutDimen.dimen_18.h,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        product.code,
-                        style: TextStyle(
-                          fontSize: LayoutDimen.dimen_18.minSp,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      InkWell(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.delete,
-                          size: LayoutDimen.dimen_30.w,
-                          color: CustomColors.neutral.c30,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: LayoutDimen.dimen_18.h,
-                  ),
-                  InputBasic(
-                    controller: TextEditingController(text: product.name),
-                    labelText: 'Nama',
-                    margin: EdgeInsets.zero,
-                  ),
-                  SizedBox(
-                    height: LayoutDimen.dimen_18.h,
-                  ),
-                  InputBasic(
-                    controller: TextEditingController(
-                      text: product.saleNet?.toString() ?? '',
-                    ),
-                    labelText: 'Harga Jual (opsional)',
-                    margin: EdgeInsets.zero,
-                  ),
-                  SizedBox(
-                    height: LayoutDimen.dimen_18.h,
-                  ),
-                  FlatButton(
-                    title: 'Ubah',
-                    onPressed: () {
-                      widget.bloc.add(
-                        // TODO(melly): handle real data
-                        UpdateProductEvent(product),
-                      );
-                    },
-                    margin: EdgeInsets.zero,
-                  ),
-                  SizedBox(
-                    height: LayoutDimen.dimen_18.h,
-                  ),
-                ],
-              ),
-            ),
-          ],
         ),
       ),
     );
---
+++ app/feature/product/lib/presentation/widgets/product_detail_content.dart
@@ -0,0 +1,138 @@
+import 'package:data_abstraction/entity/product_entity.dart';
+import 'package:feature_product/presentation/bloc/product_bloc.dart';
+import 'package:feature_product/presentation/product_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:module_common/i18n/i18n_extension.dart';
+import 'package:ui_kit/common/constants/layout_dimen.dart';
+import 'package:ui_kit/theme/colors.dart';
+import 'package:ui_kit/ui/button/flat_button.dart';
+import 'package:ui_kit/ui/input_field/input_basic.dart';
+import 'package:ui_kit/utils/screen_utils.dart';
+
+class ProductDetailContent extends StatelessWidget {
+  const ProductDetailContent({
+    super.key,
+    required this.product,
+    required this.bloc,
+  });
+  final ProductEntity product;
+  final ProductBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    String name = product.name;
+    String price = product.saleNet?.toString() ?? '';
+
+    final nameTextEditController = TextEditingController(text: name);
+    final priceTextEditController = TextEditingController(text: price);
+
+    return Padding(
+      // handle visibility of text field once keyboard shown
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Container(
+        height: LayoutDimen.dimen_352.h,
+        width: ScreenUtil.screenWidth,
+        padding: EdgeInsets.symmetric(
+          horizontal: LayoutDimen.dimen_16.w,
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(
+                vertical: LayoutDimen.dimen_12.h,
+              ),
+              width: LayoutDimen.dimen_77.w,
+              height: LayoutDimen.dimen_7.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  LayoutDimen.dimen_30.w,
+                ),
+                color: CustomColors.neutral.c90,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    height: LayoutDimen.dimen_18.h,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        product.code,
+                        style: TextStyle(
+                          fontSize: LayoutDimen.dimen_18.minSp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          // TODO(melly): implement delete event
+                        },
+                        child: Icon(
+                          Icons.delete,
+                          size: LayoutDimen.dimen_30.w,
+                          color: CustomColors.neutral.c30,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: LayoutDimen.dimen_18.h,
+                  ),
+                  InputBasic(
+                    controller: nameTextEditController,
+                    labelText: ProductStrings.nameLabelText.i18n(context),
+                    margin: EdgeInsets.zero,
+                    onChanged: (value) {
+                      name = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: LayoutDimen.dimen_18.h,
+                  ),
+                  InputBasic(
+                    controller: priceTextEditController,
+                    keyboardType: TextInputType.number,
+                    labelText: ProductStrings.priceLabelText.i18n(context),
+                    margin: EdgeInsets.zero,
+                    onChanged: (value) {
+                      price = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: LayoutDimen.dimen_18.h,
+                  ),
+                  FlatButton(
+                    title: ProductStrings.editButtonTitle.i18n(context),
+                    onPressed: () {
+                      bloc.add(
+                        UpdateProductEvent(
+                          ProductEntity(
+                            id: product.id,
+                            code: product.code,
+                            storeId: product.storeId,
+                            name: name,
+                            saleNet: price.isEmpty ? null : int.parse(price),
+                          ),
+                        ),
+                      );
+                    },
+                    margin: EdgeInsets.zero,
+                  ),
+                  SizedBox(
+                    height: LayoutDimen.dimen_18.h,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
