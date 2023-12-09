f8c86130349273382ca9944fa3d73c87b381fb70
melly-sujakto <mellysujakto@gmail.com>
2023-12-09 21:17:39 +0700

    chore: init printer setup

--- app/feature/transaction/lib/presentation/journey/sale/sales_result_page.dart
+++ app/feature/transaction/lib/presentation/journey/sale/sales_result_page.dart
@@ -1,6 +1,10 @@
+import 'package:bluetooth_print/bluetooth_print.dart';
+import 'package:bluetooth_print/bluetooth_print_model.dart';
+import 'package:data_abstraction/entity/product_entity.dart';
 import 'package:feature_transaction/common/injector/injector.dart';
 import 'package:feature_transaction/domain/navigation/interaction_navigation.dart';
 import 'package:feature_transaction/presentation/journey/sale/sale_routes.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_kit/common/constants/layout_dimen.dart';
 import 'package:ui_kit/extensions/string_extension.dart';
@@ -143,7 +147,14 @@ class _SalesResultPageState extends State<SalesResultPage> {
                               ),
                               FlatButton(
                                 title: 'Cetak Struk',
-                                onPressed: () {},
+                                onPressed: () async {
+                                  final printManager = PrintSomeText();
+                                  await printManager.scan();
+                                  await Future.delayed(
+                                    const Duration(seconds: 1),
+                                    printManager.startPrint,
+                                  );
+                                },
                                 margin: EdgeInsets.zero,
                               ),
                             ],
@@ -210,6 +221,192 @@ class _SalesResultPageState extends State<SalesResultPage> {
           }),
         );
       },
+    );
+  }
+}
+
+class PrintSomeText {
+  final bluetoothPrint = BluetoothPrint.instance;
+  List<BluetoothDevice> _devices = [];
+
+  Future<void> scan() async {
+    // begin scan
+    await bluetoothPrint.startScan(timeout: const Duration(seconds: 1));
+
+    // get devices
+    bluetoothPrint.scanResults.listen((val) {
+      _devices = val;
+      if (kDebugMode) {
+        print(_devices);
+      }
+    });
+  }
+
+  Future<void> startPrint() async {
+    await bluetoothPrint.connect(_devices.first);
+    const maxCharsPerLine = 32;
+    const separator = '-';
+    final products = [
+      ProductEntity(
+        id: 'id',
+        code: 'code',
+        name: 'name',
+        storeId: 'storeId',
+        saleNet: 10000,
+      ),
+      ProductEntity(
+        id: 'id2',
+        code: 'code2',
+        name: 'test name more than 32 charrrrrrrrrrrrrrrrrrr',
+        storeId: 'storeId2',
+        saleNet: 20000,
+      ),
+    ];
+    final textProductAndPrice = products
+        .map(
+          (e) => '${e.name}|${e.saleNet.toString().toRupiahCurrency()}',
+        )
+        .toList();
+
+    final separatorLineText = LineText(
+      type: LineText.TYPE_TEXT,
+      content:
+          List.generate(maxCharsPerLine, (index) => separator).toList().join(),
+      weight: 0,
+      align: LineText.ALIGN_CENTER,
+      linefeed: 1,
+    );
+
+    String getSpaces(int textLength) {
+      const space = ' ';
+      final numberOfSpace = maxCharsPerLine - textLength;
+      return List.generate(numberOfSpace, (index) => space).join();
+    }
+
+    String generateAlignBetween(String textContainsPipe) {
+      const minSpace = 2;
+      const maxLengthWithoutSpace = maxCharsPerLine - minSpace;
+      const pipeChar = '|';
+      final lengthWithoutPipe = textContainsPipe.length - 1;
+
+      final splitted = textContainsPipe.split(pipeChar);
+      if (splitted.length != 2) {
+        throw Exception('splitted length should be 2. seems pipe char is '
+            'there are more than one.');
+      }
+
+      if (lengthWithoutPipe > maxLengthWithoutSpace) {
+        final leftText = splitted.first;
+        final rightText = splitted.last;
+        final availableSpace = maxLengthWithoutSpace - (rightText.length);
+        final cuttedText = leftText.substring(0, availableSpace);
+        splitted[0] = cuttedText;
+      }
+
+      final spaces = getSpaces(splitted.join().length);
+
+      return splitted.join(spaces);
+    }
+
+    final listInList = textProductAndPrice
+        .map(
+          (e) => [
+            LineText(
+              type: LineText.TYPE_TEXT,
+              content: generateAlignBetween(e),
+              align: LineText.ALIGN_CENTER,
+              linefeed: 1,
+            ),
+            LineText(
+              type: LineText.TYPE_TEXT,
+              content: '   5 X      Rp.2000',
+              align: LineText.ALIGN_LEFT,
+              linefeed: 1,
+            ),
+          ],
+        )
+        .toList();
+
+    final listLineText =
+        listInList.fold(<LineText>[], (previousValue, element) {
+      previousValue.addAll(element);
+      return previousValue;
+    });
+
+    final List<LineText> list = [
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: 'Toko Adi Jaya Sembako',
+        weight: 1,
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content:
+            'Jembatan 14, RT.001/RW.005, Bojong Rawalumbu, Kec. Rawalumbu, Kota Bks, Jawa Barat',
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: '081234567890',
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+      ),
+      separatorLineText,
+      ...listLineText,
+      separatorLineText,
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: generateAlignBetween('Total|Rp.20.000'),
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+        weight: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: generateAlignBetween('Bayar|Rp.20.000'),
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: generateAlignBetween('Kembali|Rp.0'),
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+      ),
+      separatorLineText,
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: 'Tanggal Transaksi',
+        align: LineText.ALIGN_LEFT,
+        linefeed: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: 'Senin, 6 November 2023',
+        align: LineText.ALIGN_LEFT,
+        linefeed: 1,
+        weight: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: generateAlignBetween('Waktu|Kasir'),
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+      ),
+      LineText(
+        type: LineText.TYPE_TEXT,
+        content: generateAlignBetween('21:22|Melly Sujakto'),
+        align: LineText.ALIGN_CENTER,
+        linefeed: 1,
+        weight: 1,
+      ),
+    ];
+    await Future.delayed(
+      const Duration(seconds: 2),
+      () => bluetoothPrint.printReceipt({}, list),
     );
   }
 }
