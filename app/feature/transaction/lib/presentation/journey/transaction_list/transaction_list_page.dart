f286bdc8aab3763292e60b295cb152cc3c61dffa
melly-sujakto <mellysujakto@gmail.com>
2023-12-16 15:14:02 +0700

    chore: add filter widget

--- app/feature/transaction/lib/presentation/journey/transaction_list/transaction_list_page.dart
+++ app/feature/transaction/lib/presentation/journey/transaction_list/transaction_list_page.dart
@@ -32,78 +32,8 @@ class _TransactionListPageState extends State<TransactionListPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              margin: EdgeInsets.all(LayoutDimen.dimen_16.w),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(LayoutDimen.dimen_10.w),
-                color: CustomColors.neutral.c90,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: InkWell(
-                      onTap: () {
-                        if (!showSales) {
-                          setState(() {
-                            showSales = true;
-                          });
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: LayoutDimen.dimen_12.h,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(LayoutDimen.dimen_10.w),
-                          color: showSales ? CustomColors.secondary.c60 : null,
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Penjualan',
-                            style: TextStyle(
-                              fontSize: LayoutDimen.dimen_16.minSp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: InkWell(
-                      onTap: () {
-                        if (showSales) {
-                          setState(() {
-                            showSales = false;
-                          });
-                        }
-                      },
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: LayoutDimen.dimen_12.h,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.circular(LayoutDimen.dimen_10.w),
-                          color: !showSales ? CustomColors.secondary.c60 : null,
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Stok Masuk',
-                            style: TextStyle(
-                              fontSize: LayoutDimen.dimen_16.minSp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            buttonController(),
+            dateController(),
             showSales
                 ? percentIndicator([
                     'Rp.247.650.000',
@@ -113,10 +43,167 @@ class _TransactionListPageState extends State<TransactionListPage> {
                 : percentIndicator([
                     'Rp.197.650.000',
                     '1411 pcs',
-                    '155 stok masuuk',
+                    '155 stok masuk',
                   ])
           ],
         ),
+      ),
+    );
+  }
+
+  Widget dateController() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: LayoutDimen.dimen_35.h,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(50),
+            child: Material(
+              elevation: 1,
+              color: CustomColors.neutral.c80.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(50),
+              child: Icon(
+                Icons.arrow_left_rounded,
+                size: LayoutDimen.dimen_35.h,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: LayoutDimen.dimen_11.w,
+            ),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(
+                LayoutDimen.dimen_10.w,
+              ),
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: LayoutDimen.dimen_11.w,
+                  vertical: LayoutDimen.dimen_5.h,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    LayoutDimen.dimen_10.w,
+                  ),
+                  color: CustomColors.errorAccent.c50.withOpacity(0.3),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'November',
+                      style: TextStyle(
+                        fontSize: LayoutDimen.dimen_18.minSp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '2023',
+                      style: TextStyle(
+                        fontSize: LayoutDimen.dimen_14.minSp,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            borderRadius: BorderRadius.circular(50),
+            child: Material(
+              elevation: 1,
+              color: CustomColors.neutral.c80.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(50),
+              child: Icon(
+                Icons.arrow_right_rounded,
+                size: LayoutDimen.dimen_35.h,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buttonController() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: LayoutDimen.dimen_16.w,
+        right: LayoutDimen.dimen_16.w,
+        top: LayoutDimen.dimen_16.h,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(LayoutDimen.dimen_10.w),
+        color: CustomColors.neutral.c90,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                if (!showSales) {
+                  setState(() {
+                    showSales = true;
+                  });
+                }
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: LayoutDimen.dimen_12.h,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(LayoutDimen.dimen_10.w),
+                  color: showSales ? CustomColors.secondary.c60 : null,
+                ),
+                child: Center(
+                  child: Text(
+                    'Penjualan',
+                    style: TextStyle(
+                      fontSize: LayoutDimen.dimen_16.minSp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
+                if (showSales) {
+                  setState(() {
+                    showSales = false;
+                  });
+                }
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  vertical: LayoutDimen.dimen_12.h,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(LayoutDimen.dimen_10.w),
+                  color: !showSales ? CustomColors.secondary.c60 : null,
+                ),
+                child: Center(
+                  child: Text(
+                    'Stok Masuk',
+                    style: TextStyle(
+                      fontSize: LayoutDimen.dimen_16.minSp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
