70ef99a0881e38640bacb2305385288d4f419815
melly-sujakto <mellysujakto@gmail.com>
2023-12-16 14:11:52 +0700

    chore: implement button transaction controller

--- app/feature/transaction/lib/presentation/journey/transaction_list/transaction_list_page.dart
+++ app/feature/transaction/lib/presentation/journey/transaction_list/transaction_list_page.dart
@@ -4,75 +4,171 @@ import 'package:ui_kit/common/constants/layout_dimen.dart';
 import 'package:ui_kit/theme/colors.dart';
 import 'package:ui_kit/utils/screen_utils.dart';
 
-class TransactionListPage extends StatelessWidget {
+class TransactionListPage extends StatefulWidget {
   const TransactionListPage({super.key});
 
   @override
+  State<TransactionListPage> createState() => _TransactionListPageState();
+}
+
+class _TransactionListPageState extends State<TransactionListPage> {
+  bool showSales = true;
+  final circleWidth = LayoutDimen.dimen_28.w;
+
+  @override
   Widget build(BuildContext context) {
-    final circleWidth = LayoutDimen.dimen_28.w;
     return Scaffold(
+      backgroundColor: CustomColors.neutral.c98,
       appBar: AppBar(
-        title: const Text('Circular Percent Indicators'),
+        title: Text(
+          'Transaksi',
+          style: TextStyle(
+            fontSize: LayoutDimen.dimen_19.minSp,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Center(
-              child: CircularPercentIndicator(
-                radius: LayoutDimen.dimen_150.w,
-                lineWidth: circleWidth,
-                animation: true,
-                percent: 1,
-                center: Padding(
-                  padding: EdgeInsets.all(
-                    circleWidth + LayoutDimen.dimen_16.w,
-                  ),
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: Text(
-                          'Rp.247.650.000',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: LayoutDimen.dimen_24.minSp,
+            Container(
+              margin: EdgeInsets.all(LayoutDimen.dimen_16.w),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(LayoutDimen.dimen_10.w),
+                color: CustomColors.neutral.c90,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        if (!showSales) {
+                          setState(() {
+                            showSales = true;
+                          });
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: LayoutDimen.dimen_12.h,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(LayoutDimen.dimen_10.w),
+                          color: showSales ? CustomColors.secondary.c60 : null,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Penjualan',
+                            style: TextStyle(
+                              fontSize: LayoutDimen.dimen_16.minSp,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ),
                       ),
-                      Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              '1100 pcs',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: LayoutDimen.dimen_16.minSp,
-                              ),
+                    ),
+                  ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        if (showSales) {
+                          setState(() {
+                            showSales = false;
+                          });
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: LayoutDimen.dimen_12.h,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(LayoutDimen.dimen_10.w),
+                          color: !showSales ? CustomColors.secondary.c60 : null,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Stok Masuk',
+                            style: TextStyle(
+                              fontSize: LayoutDimen.dimen_16.minSp,
+                              fontWeight: FontWeight.w600,
                             ),
-                            Text(
-                              '297 penjualan',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w200,
-                                fontSize: LayoutDimen.dimen_14.minSp,
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-                // footer: const Text(
-                //   'Sales this week',
-                //   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-                // ),
-                // circularStrokeCap: CircularStrokeCap.round,
-                progressColor: CustomColors.errorAccent.c70,
+                ],
               ),
             ),
+            showSales
+                ? percentIndicator([
+                    'Rp.247.650.000',
+                    '1100 pcs',
+                    '297 penjualan',
+                  ])
+                : percentIndicator([
+                    'Rp.197.650.000',
+                    '1411 pcs',
+                    '155 stok masuuk',
+                  ])
           ],
         ),
+      ),
+    );
+  }
+
+  Widget percentIndicator(List<String> texts) {
+    return Center(
+      child: CircularPercentIndicator(
+        radius: LayoutDimen.dimen_150.w,
+        animationDuration: 1000,
+        lineWidth: circleWidth,
+        animation: true,
+        percent: 1,
+        center: Padding(
+          padding: EdgeInsets.all(
+            circleWidth + LayoutDimen.dimen_16.w,
+          ),
+          child: Stack(
+            children: [
+              Center(
+                child: Text(
+                  texts.first,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: LayoutDimen.dimen_24.minSp,
+                  ),
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      texts[1],
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: LayoutDimen.dimen_16.minSp,
+                      ),
+                    ),
+                    Text(
+                      texts.last,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        fontSize: LayoutDimen.dimen_14.minSp,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        progressColor: CustomColors.errorAccent.c70,
       ),
     );
   }
