f5268d3f85529e7bc2dfa308c645e62c8727611f
melly-sujakto <melly.sujakto@dkatalis.com>
2024-03-09 20:54:56 +0700

    chore: add sh script to handle switch env

---
+++ app/app_stokme/tools/env.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# How to run?
+# => ./env.sh env=dev
+
+if [ $# -eq 0 ]; then
+    echo "[Error] please run command includes: $0 env=..."
+    exit 1
+fi
+
+# Extract the value of 'env' from the argument
+ENV_PARAM=$(echo "$1" | cut -d'=' -f2)
+
+if [ "$ENV_PARAM" == "prod" ]; then
+    ENV="stokme"
+elif [ "$ENV_PARAM" == "staging" ]; then
+    ENV="stokme-staging"
+else
+    ENV="stokme-dev"
+fi
+
+flutterfire configure --project=$ENV --platforms=android,ios -y
+exit 0
