6ed14046332121c653730c1df1f9bd44ee085552
melly-sujakto <melly.sujakto@dkatalis.com>
2024-03-19 01:49:14 +0700

    chore: move printer util into module common

--- app/generic/module_common/lib/common/utils/printer_util.dart
+++ app/generic/module_common/lib/common/utils/printer_util.dart
@@ -1,26 +1,31 @@
-import 'package:bluetooth_print/bluetooth_print.dart';
-import 'package:bluetooth_print/bluetooth_print_model.dart';
-import 'package:flutter/foundation.dart';
+import 'package:module_common/package/bluetooth_print.dart';
 
 class PrinterUtil {
   final bluetoothPrint = BluetoothPrint.instance;
-  List<BluetoothDevice> _devices = [];
 
-  Future<void> scan() async {
+  Future<List<BluetoothDevice>> scan() async {
+    List<BluetoothDevice>? devices;
+
     // begin scan
     await bluetoothPrint.startScan(timeout: const Duration(seconds: 1));
 
     // get devices
-    bluetoothPrint.scanResults.listen((val) {
-      _devices = val;
-      if (kDebugMode) {
-        print(_devices);
-      }
+    bluetoothPrint.scanResults.listen((bluetoothDevices) {
+      devices = bluetoothDevices;
     });
+
+    while (devices == null) {
+      await Future.delayed(const Duration(milliseconds: 100));
+    }
+
+    return devices!;
   }
 
-  Future<void> startPrint(List<LineText> lineTexts) async {
-    await bluetoothPrint.connect(_devices.first);
+  Future<void> startPrint({
+    required BluetoothDevice device,
+    required List<LineText> lineTexts,
+  }) async {
+    await bluetoothPrint.connect(device);
 
     bool isConnected = false;
     int maxCount = 0;
---
+++ app/generic/module_common/lib/package/bluetooth_print.dart
@@ -0,0 +1,2 @@
+export 'package:bluetooth_print/bluetooth_print.dart';
+export 'package:bluetooth_print/bluetooth_print_model.dart';
