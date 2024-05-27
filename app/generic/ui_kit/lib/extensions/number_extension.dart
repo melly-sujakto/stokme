5a3983b84e32149991fbecfd0f80934c4738a6d6
melly-sujakto <melly.sujakto@dkatalis.com>
2024-05-27 20:26:04 +0700

    chore: add decimalDigits param

--- app/generic/ui_kit/lib/extensions/number_extension.dart
+++ app/generic/ui_kit/lib/extensions/number_extension.dart
@@ -1,11 +1,11 @@
 import 'package:module_common/package/intl.dart';
 
 extension NumberExtension on num {
-  String toRupiahCurrency() {
+  String toRupiahCurrency({int decimalDigits = 2}) {
     return NumberFormat.currency(
       locale: 'id',
       symbol: 'Rp.',
-      decimalDigits: 2,
+      decimalDigits: decimalDigits,
     ).format(this);
   }
 }
