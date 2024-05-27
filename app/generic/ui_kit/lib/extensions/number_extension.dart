7a2d4b9ab91b278aba69def3b2aa870444eb2fd8
melly-sujakto <melly.sujakto@dkatalis.com>
2024-05-27 23:53:21 +0700

    chore: make default is 0

--- app/generic/ui_kit/lib/extensions/number_extension.dart
+++ app/generic/ui_kit/lib/extensions/number_extension.dart
@@ -1,7 +1,7 @@
 import 'package:module_common/package/intl.dart';
 
 extension NumberExtension on num {
-  String toRupiahCurrency({int decimalDigits = 2}) {
+  String toRupiahCurrency({int decimalDigits = 0}) {
     return NumberFormat.currency(
       locale: 'id',
       symbol: 'Rp.',
