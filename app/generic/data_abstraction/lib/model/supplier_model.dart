c039780a28f7e22e5cc728174fa685ffea63f6c9
melly-sujakto <melly.sujakto@dkatalis.com>
2024-06-03 00:08:14 +0700

    chore: add from entity factory and toFirestoreJson function

--- app/generic/data_abstraction/lib/model/supplier_model.dart
+++ app/generic/data_abstraction/lib/model/supplier_model.dart
@@ -28,4 +28,36 @@ class SupplierModel extends SupplierEntity {
       updatedBy: json['updated_by'],
     );
   }
+
+  factory SupplierModel.fromEntity(SupplierEntity entity) {
+    return SupplierModel(
+      id: entity.id,
+      name: entity.name,
+      phone: entity.phone,
+      isActive: entity.isActive,
+      createdAt: entity.createdAt,
+      createdBy: entity.createdBy,
+      updatedAt: entity.updatedAt,
+      updatedBy: entity.updatedBy,
+    );
+  }
+
+  Map<String, dynamic> toFirestoreJson(
+    String overridedStoreId, {
+    DateTime? overridedCreatedAt,
+    String? overridedCreatedBy,
+    DateTime? overridedUpdatedAt,
+    String? overridedUpdatedBy,
+  }) {
+    return {
+      'phone': phone,
+      'name': name,
+      'store_id': overridedStoreId,
+      'is_active': isActive,
+      'created_at': (overridedCreatedAt ?? createdAt)?.millisecondsSinceEpoch,
+      'created_by': overridedCreatedBy ?? createdBy,
+      'updated_at': (overridedUpdatedAt ?? updatedAt)?.millisecondsSinceEpoch,
+      'updated_by': overridedUpdatedBy ?? updatedBy,
+    };
+  }
 }
