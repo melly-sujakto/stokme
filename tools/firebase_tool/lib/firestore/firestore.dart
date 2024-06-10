8b2354043322e6ea02dd5a79d96f18960061e9d2
melly-sujakto <melly.sujakto@dkatalis.com>
2024-06-10 20:34:47 +0700

    chore: put note

--- tools/firebase_tool/lib/firestore/firestore.dart
+++ tools/firebase_tool/lib/firestore/firestore.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:firebase_tool/firestore/firestore_service.dart';
 
+// How to run?
+// => dart run ./lib/firestore/firestore.dart "<email>" "<password>" "<collection_name>"
 void main(List<String> args) async {
   final service = await FirestoreService().initAndSignIn(
     email: args[0],
